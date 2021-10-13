601529ff8b05d9c098549b5103d7a32c83bee506
049er <werner@scholtzonline.net>
2021-10-13 14:34:59 +0200

    init commit

---
+++ lib/VisionDetectorViews/barcode_scanner_view.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'camera_view.dart';
+import 'painters/barcode_detector_painter.dart';
+
+class BarcodeScannerView extends StatefulWidget {
+  @override
+  _BarcodeScannerViewState createState() => _BarcodeScannerViewState();
+}
+
+class _BarcodeScannerViewState extends State<BarcodeScannerView> {
+  BarcodeScanner barcodeScanner =
+      GoogleMlKit.vision.barcodeScanner([BarcodeFormat.qrCode]);
+
+  bool isBusy = false;
+  CustomPaint? customPaint;
+
+  @override
+  void dispose() {
+    barcodeScanner.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CameraView(
+      title: 'Barcode Scanner',
+      customPaint: customPaint,
+      onImage: (inputImage) {
+        processImage(inputImage);
+      },
+    );
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (isBusy) return;
+    isBusy = true;
+    final barcodes = await barcodeScanner.processImage(inputImage);
+    //print('Found ${barcodes.length} barcodes');
+
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null) {
+      final painter = BarcodeDetectorPainter(
+          barcodes,
+          inputImage.inputImageData!.size,
+          inputImage.inputImageData!.imageRotation);
+
+      if (barcodes.length >= 2) {
+        print(painter.absoluteImageSize);
+        print(painter.rotation);
+        print(painter.barcodes);
+      }
+
+      customPaint = CustomPaint(painter: painter);
+    } else {
+      customPaint = null;
+    }
+    isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
