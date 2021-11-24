cd2f7b501d7ee34100598bd45adecb81aca4d891
049er <werner@scholtzonline.net>
2021-11-24 10:52:48 +0200

    Experimental

---
+++ lib/database/dataProcessors/barcode_raw_data_injector.dart
@@ -0,0 +1,113 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_google_ml_kit/VisionDetectorViews/painters/coordinates_translator.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:hive/hive.dart';
+import '../databaseAdapters/raw_data_adapter.dart';
+
+class BarcodeDatabaseInjector {
+  BarcodeDatabaseInjector(this.barcodes, this.absoluteImageSize, this.rotation);
+
+  final List<Barcode> barcodes;
+  final Size absoluteImageSize;
+  final InputImageRotation rotation;
+}
+
+injectBarcode(
+  BuildContext context,
+  List<Barcode> barcodes,
+  Size absoluteImageSize,
+  InputImageRotation rotation,
+  Box<dynamic> rawDataBox,
+) {
+  var qrCodeData = [];
+
+  double roundDouble(double val, int places) {
+    num mod = pow(10.0, places);
+    return ((val * mod).round().toDouble() / mod);
+  }
+
+  Size size = Size(
+      MediaQuery.of(context).size.width, MediaQuery.of(context).size.height);
+
+  for (final Barcode barcode in barcodes) {
+    final barcodeBoundingBoxLeft = translateX(
+        barcode.value.boundingBox!.left, rotation, size, absoluteImageSize);
+    final barcodeBoundingBoxRight = translateX(
+        barcode.value.boundingBox!.right, rotation, size, absoluteImageSize);
+    final barcodeBoundingBoxTop = translateY(
+        barcode.value.boundingBox!.top, rotation, size, absoluteImageSize);
+    final barcodeBoundingBoxBottom = translateY(
+        barcode.value.boundingBox!.bottom, rotation, size, absoluteImageSize);
+
+    var barcodeCenterPoint =
+        ((barcodeBoundingBoxLeft - barcodeBoundingBoxRight).abs() +
+                (barcodeBoundingBoxTop - barcodeBoundingBoxBottom).abs()) /
+            2;
+
+    var distanceFromCamera =
+        (4341 / barcodeCenterPoint) - 15.75; //Specifically for redmi Note10S
+
+    var barcodeCenterVector = [
+      ((barcodeBoundingBoxLeft + barcodeBoundingBoxRight) / 2),
+      ((barcodeBoundingBoxTop + barcodeBoundingBoxBottom) / 2)
+    ];
+
+    double mmX = 70 / (barcodeBoundingBoxLeft - barcodeBoundingBoxRight).abs();
+    double mmY = 70 / (barcodeBoundingBoxTop - barcodeBoundingBoxBottom).abs();
+    // debugPrint(
+    //     'qrCodeData: ${barcode.value.displayValue}, ${barcodeCenterVector}, ${distanceFromCamera}, ${mmX}, ${mmY}');
+    qrCodeData.add([
+      barcode.value.displayValue,
+      barcodeCenterVector,
+      distanceFromCamera,
+      mmX,
+      mmY
+    ]);
+  }
+  //print(qrCodeData);
+  if (qrCodeData.length >= 2) {
+    for (var i = 0; i < qrCodeData.length; i++) {
+      var vectorBetweenBarcodesX, vectorBetweenBarcodesY;
+      double disX, disY;
+      int _now = DateTime.now().millisecondsSinceEpoch;
+
+      if (i < qrCodeData.length - 1) {
+        vectorBetweenBarcodesX = qrCodeData[i][1][0] - qrCodeData[i + 1][1][0];
+        vectorBetweenBarcodesY = qrCodeData[i][1][1] - qrCodeData[i + 1][1][1];
+
+        var x = ((qrCodeData[i][3] + qrCodeData[i + 1][3]) / 2);
+        var y = ((qrCodeData[i][4] + qrCodeData[i + 1][4]) / 2);
+
+        disX = -roundDouble((x * vectorBetweenBarcodesX), 2);
+        disY = -roundDouble((y * vectorBetweenBarcodesY), 2);
+
+        var uid = "${qrCodeData[i][0]}_${qrCodeData[i + 1][0]}";
+        uid.replaceAll(' ', '');
+
+        var qrCodesVector =
+            QrCodes(uid: uid, X: disX, Y: disY, createdDated: _now);
+
+        rawDataBox.put(uid, qrCodesVector);
+      } else {
+        vectorBetweenBarcodesX = qrCodeData[i][1][0] - qrCodeData[0][1][0];
+        vectorBetweenBarcodesY = qrCodeData[i][1][1] - qrCodeData[0][1][1];
+
+        var x = ((qrCodeData[i][3] + qrCodeData[0][3]) / 2);
+        var y = ((qrCodeData[i][4] + qrCodeData[0][4]) / 2);
+
+        disX = -roundDouble((x * vectorBetweenBarcodesX), 2);
+        disY = -roundDouble((y * vectorBetweenBarcodesY), 2);
+
+        var uid = "${qrCodeData[i][0]}_${qrCodeData[0][0]}";
+        uid.replaceAll(' ', '');
+        var qrCodesVector =
+            QrCodes(uid: uid, X: disX, Y: disY, createdDated: _now);
+
+        rawDataBox.put(uid, qrCodesVector);
+      }
+    }
+  }
+}
---
+++ lib/database/wipDatabaseViews/wip_hive_database_consolidation.dart
@@ -0,0 +1,265 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_google_ml_kit/widgets/alert_dialog_widget.dart';
+import 'package:hive/hive.dart';
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../databaseAdapters/consolidated_data_adapter.dart';
+
+class WipHiveDatabaseConsolidationView extends StatefulWidget {
+  const WipHiveDatabaseConsolidationView({Key? key}) : super(key: key);
+
+  @override
+  _WipHiveDatabaseConsolidationViewState createState() =>
+      _WipHiveDatabaseConsolidationViewState();
+}
+
+class _WipHiveDatabaseConsolidationViewState
+    extends State<WipHiveDatabaseConsolidationView> {
+  List displayList = [];
+  List fixedPoints = ['1'];
+  List processedDataList = [];
+  Map<String, List> processedDataMap = {};
+  Map<String, List> consolidatedDataMap = {};
+  Map<String, List> currentPoints = {};
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  @override
+  void initState() {
+    super.initState();
+    displayList.clear();
+    fixedPoints.clear();
+    _getPrefs(_prefs, fixedPoints);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            FloatingActionButton(
+              //Clear Database
+              heroTag: null,
+              onPressed: () async {
+                displayList.clear();
+                var consolidatedDataBox =
+                    await Hive.openBox('consolidatedDataBox');
+                var pro = await Hive.openBox('processedDataBox');
+                consolidatedDataBox.clear();
+                pro.clear();
+                showMyAboutDialog(context, "Deleted Database");
+              },
+              child: const Icon(Icons.delete),
+            ),
+            FloatingActionButton(
+              heroTag: null,
+              onPressed: () {
+                fixedPointsDialog(context, fixedPoints, _prefs);
+              },
+              child: const Icon(Icons.change_circle),
+            ),
+            FloatingActionButton(
+              heroTag: null,
+              onPressed: () {
+                displayList.clear();
+                setState(() {});
+              },
+              child: const Icon(Icons.refresh),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text('Hive Database 2D'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: FutureBuilder<List>(
+        future: consolidatingData(displayList),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Column(
+              children: [
+                Form(child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                )),
+                const Center(child: CircularProgressIndicator()),
+              ],
+            );
+          } else {
+            List myList = snapshot.data ?? [];
+            return ListView.builder(
+                itemCount: myList.length,
+                itemBuilder: (context, index) {
+                  var myText = myList[index]
+                      .toString()
+                      .replaceAll(RegExp(r'\[|\]'), '')
+                      .replaceAll(' ', '')
+                      .split(',')
+                      .toList();
+
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        child: Text(myText[0], textAlign: TextAlign.start),
+                        width: 30,
+                      ),
+                      SizedBox(
+                        child: Text(myText[1], textAlign: TextAlign.start),
+                        width: 75,
+                      ),
+                      SizedBox(
+                        child: Text(myText[2], textAlign: TextAlign.start),
+                        width: 75,
+                      ),
+                      SizedBox(
+                        child: Text(myText[3], textAlign: TextAlign.start),
+                        width: 150,
+                      ),
+                    ],
+                  );
+                });
+          }
+        },
+      ),
+    );
+  }
+
+  Future<List> consolidatingData(List displayList) async {
+    displayList.clear();
+    currentPoints.clear();
+
+    var processedDataBox = await Hive.openBox('processedDataBox');
+    var consolidatedDataBox = await Hive.openBox('WipConsolidatedDataBox');
+
+    _generateProcessedDataMap(processedDataBox, processedDataMap);
+    debugPrint('processedDataMap: ${processedDataMap.toIMap()}');
+    _generateConsolidatedDataMap(processedDataMap, consolidatedDataMap);
+
+    //_displayList(consolidatedDataMap, displayList, consolidatedDataBox);
+    return displayList;
+  }
+}
+
+_displayList(
+    Map consolidatedDataMap, List displayList, Box consolidatedDataBox) {
+  for (var i = 0; i < consolidatedDataMap.length; i++) {
+    displayList.add([
+      consolidatedDataMap.keys.elementAt(i),
+      consolidatedDataMap.values.elementAt(i)[0],
+      consolidatedDataMap.values.elementAt(i)[1],
+      consolidatedDataMap.values.elementAt(i)[2]
+    ]);
+
+    var data = ConsolidatedData(
+        uid: consolidatedDataMap.keys.elementAt(i),
+        X: consolidatedDataMap.values.elementAt(i)[0],
+        Y: consolidatedDataMap.values.elementAt(i)[1],
+        timeStamp:
+            int.parse(consolidatedDataMap.values.elementAt(i)[2].toString()),
+        fixed: false);
+    consolidatedDataBox.put(data.uid, data);
+  }
+  displayList.sort((a, b) => a[0].compareTo(b[0]));
+
+  //print('consolidatedDataBox: ${consolidatedDataBox.toMap().toIMap()}');
+  //print('displayList: ${displayList.toIList()}');
+}
+
+double roundDouble(double val, int places) {
+  num mod = pow(10.0, places);
+  return ((val * mod).round().toDouble() / mod);
+}
+
+_generateProcessedDataMap(Box processedDataBox, Map processedDataMap) {
+  var _processedDataBox = processedDataBox.toMap();
+  _processedDataBox.forEach((key, value) {
+    List qrCodeData = value
+        .toString()
+        .replaceAll(RegExp(r'\[\]'), '')
+        //.replaceAll('_', ',')
+        .replaceAll(' ', '')
+        .split(',')
+        .toList();
+    var qrCodeID = qrCodeData[0].toString().split('_').toList();
+    processedDataMap.putIfAbsent(
+        qrCodeData[0],
+        () => [
+              qrCodeID[0],
+              qrCodeID[1],
+              qrCodeData[1],
+              qrCodeData[2],
+              qrCodeData[3]
+            ]);
+  });
+  //debugPrint('qrCodeData: ${processedDataMap.toIMap()}');
+}
+
+_generateConsolidatedDataMap(Map processedDataMap, Map consolidatedDataMap) {}
+
+_getPrefs(Future<SharedPreferences> _prefs, List fixedPoints) async {
+  final SharedPreferences prefs = await _prefs;
+  fixedPoints.clear();
+  fixedPoints.add(prefs.getString('fixedPoints') ?? '1');
+}
+
+_setPrefs(Future<SharedPreferences> _prefs, List fixedPoints) async {
+  final SharedPreferences prefs = await _prefs;
+  prefs.setString('fixedPoints', fixedPoints[0]);
+}
+
+fixedPointsDialog(BuildContext context, List fixedPoints,
+    Future<SharedPreferences> _prefs) async {
+  String dropdownValue = fixedPoints[0];
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Pick Fixed Point'),
+        content: SingleChildScrollView(
+            child: DropdownButton(
+                value: dropdownValue,
+                items: <String>['1', '2', '3', '4', '5', '6', '7']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  fixedPoints.clear();
+                  fixedPoints.add(newValue);
+
+                  dropdownValue = newValue!;
+                  _setPrefs(_prefs, fixedPoints);
+                })),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Ok'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
