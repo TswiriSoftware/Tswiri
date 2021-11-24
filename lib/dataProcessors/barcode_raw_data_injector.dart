2d2fc06686ba77e8d21345f791e9a9bf0d425d25
Spodeopieter <pieter.scholtz@gmail.com>
2021-11-24 10:51:01 +0200

    Refactoring and cleanup of barcode injector

--- lib/dataProcessors/barcode_raw_data_injector.dart
+++ lib/dataProcessors/barcode_raw_data_injector.dart
@@ -25,8 +25,8 @@ injectBarcode(
   //print('Hello @049er');
   //print('Hi @Spodeo');
 
-  var barcodeCenterPoints = []; // Centre co-ordinates of scanned QR codes
-  var qrCodeData = [];
+  //var barcodeCenterPoints = []; // Centre co-ordinates of scanned QR codes
+  List<QrCode> qrCodes = [];
 
   //Method to round double values
   double roundDouble(double val, int places) {
@@ -34,69 +34,56 @@ injectBarcode(
     return ((val * mod).round().toDouble() / mod);
   }
 
-  Size size = Size(
-      MediaQuery.of(context).size.width, MediaQuery.of(context).size.height);
-
   for (final Barcode barcode in barcodes) {
-    final barcodeBoundingBoxLeft = translateX(
-        barcode.value.boundingBox!.left, rotation, size, absoluteImageSize);
-    final barcodeBoundingBoxRight = translateX(
-        barcode.value.boundingBox!.right, rotation, size, absoluteImageSize);
-    final barcodeBoundingBoxTop = translateY(
-        barcode.value.boundingBox!.top, rotation, size, absoluteImageSize);
-    final barcodeBoundingBoxBottom = translateY(
-        barcode.value.boundingBox!.bottom, rotation, size, absoluteImageSize);
 
-    var barcodePixelDistanceX =
-        (barcodeBoundingBoxLeft + barcodeBoundingBoxRight) / 2;
-    var barcodePixelDistanceY =
-        (barcodeBoundingBoxTop + barcodeBoundingBoxBottom) / 2;
-    barcodeCenterPoints
-        .add(Offset(barcodePixelDistanceX, barcodePixelDistanceY));
+    
+    if (barcode.value.displayValue != null && barcode.value.boundingBox != null) {
 
-    var barcodeCenterPoint =
-        ((barcodeBoundingBoxLeft - barcodeBoundingBoxRight).abs() +
-                (barcodeBoundingBoxTop - barcodeBoundingBoxBottom).abs()) /
-            2;
+    Point barcodeCenterPoint = Point(
+      (barcode.value.boundingBox!.left + barcode.value.boundingBox!.right)/2,
+      (barcode.value.boundingBox!.top + barcode.value.boundingBox!.bottom)/2
+    );
 
-    var distanceFromCamera =
-        (4341 / barcodeCenterPoint) - 15.75; //Specifically for redmi Note10S
+    QrCode qrCode = QrCode(barcode.value.displayValue!,barcodeCenterPoint);
 
-    var barcodeCenterVector = [
-      ((barcodeBoundingBoxLeft + barcodeBoundingBoxRight) / 2),
-      ((barcodeBoundingBoxTop + barcodeBoundingBoxBottom) / 2)
-    ];
+    
+    qrCode.distanceFromCamera = calaculateDistanceFormCamera(barcode.value.boundingBox!,barcodeCenterPoint); //Specifically for redmi Note10S
 
-    double mmX = 70 / (barcodeBoundingBoxLeft - barcodeBoundingBoxRight).abs();
-    double mmY = 70 / (barcodeBoundingBoxTop - barcodeBoundingBoxBottom).abs();
-    // debugPrint(
-    //     'qrCodeData: ${barcode.value.displayValue}, ${barcodeCenterVector}, ${distanceFromCamera}, ${mmX}, ${mmY}');
-    qrCodeData.add([
-      barcode.value.displayValue,
-      barcodeCenterVector,
-      distanceFromCamera,
-      mmX,
-      mmY
-    ]);
+    qrCodes.add(qrCode);
+    }
+
+    else {
+      throw Exception('Barcode with null displayvalue or boundingbox detected ');
+    }
+
+
   }
   //print(qrCodeData);
-  if (qrCodeData.length >= 2) {
-    for (var i = 0; i < qrCodeData.length; i++) {
+  if (qrCodes.length >= 2) {
+
+    // for (List<dynamic> qrCode in qrCodes) {
+    //   //qrCode
+    // };
+
+    // qrCodes.forEach((element) { print(element.);});
+
+
+    for (var i = 0; i < qrCodes.length; i++) {
       var vectorBetweenBarcodesX, vectorBetweenBarcodesY;
       double disX, disY;
       int _now = DateTime.now().millisecondsSinceEpoch;
 
-      if (i < qrCodeData.length - 1) {
-        vectorBetweenBarcodesX = qrCodeData[i][1][0] - qrCodeData[i + 1][1][0];
-        vectorBetweenBarcodesY = qrCodeData[i][1][1] - qrCodeData[i + 1][1][1];
+      if (i < qrCodes.length - 1) {
+        vectorBetweenBarcodesX = qrCodes[i][1][0] - qrCodes[i + 1][1][0];
+        vectorBetweenBarcodesY = qrCodes[i][1][1] - qrCodes[i + 1][1][1];
 
-        var x = ((qrCodeData[i][3] + qrCodeData[i + 1][3]) / 2);
-        var y = ((qrCodeData[i][4] + qrCodeData[i + 1][4]) / 2);
+        var x = ((qrCodes[i][3] + qrCodes[i + 1][3]) / 2);
+        var y = ((qrCodes[i][4] + qrCodes[i + 1][4]) / 2);
 
         disX = roundDouble((x * vectorBetweenBarcodesX), 1);
         disY = roundDouble((y * vectorBetweenBarcodesY), 1);
 
-        var uid = "${qrCodeData[i][0]}_${qrCodeData[i + 1][0]}";
+        var uid = "${qrCodes[i][0]}_${qrCodes[i + 1][0]}";
         uid.replaceAll(' ', '');
 
         var qrCodesVector =
@@ -104,22 +91,46 @@ injectBarcode(
 
         rawDataBox.put(uid, qrCodesVector);
       } else {
-        vectorBetweenBarcodesX = qrCodeData[i][1][0] - qrCodeData[0][1][0];
-        vectorBetweenBarcodesY = qrCodeData[i][1][1] - qrCodeData[0][1][1];
+        vectorBetweenBarcodesX = qrCodes[i][1][0] - qrCodes[0][1][0];
+        vectorBetweenBarcodesY = qrCodes[i][1][1] - qrCodes[0][1][1];
 
-        var x = ((qrCodeData[i][3] + qrCodeData[0][3]) / 2);
-        var y = ((qrCodeData[i][4] + qrCodeData[0][4]) / 2);
+        var x = ((qrCodes[i][3] + qrCodes[0][3]) / 2);
+        var y = ((qrCodes[i][4] + qrCodes[0][4]) / 2);
 
         disX = roundDouble((x * vectorBetweenBarcodesX), 1);
         disY = roundDouble((y * vectorBetweenBarcodesY), 1);
 
-        var uid = "${qrCodeData[i][0]}_${qrCodeData[0][0]}";
+        var uid = "${qrCodes[i][0]}_${qrCodes[0][0]}";
         uid.replaceAll(' ', '');
         var qrCodesVector =
             QrCodes(uid: uid, X: disX, Y: disY, createdDated: _now);
+            //UID 1_2 , 
 
         rawDataBox.put(uid, qrCodesVector);
       }
     }
   }
+}
+
+double calaculateDistanceFormCamera(Rect boundingBox,Point barcodeCenterPoint) {
+   var barcodeCenterPoint =
+       ((boundingBox.left - boundingBox.right).abs() +
+               (boundingBox.top - boundingBox.bottom).abs()) /
+           2;
+  
+  var distanceFromCamera =
+      (4341 / barcodeCenterPoint) - 15.75; //Specifically for redmi Note10S
+
+  //TODO: implement calibration lookup table 
+
+  return distanceFromCamera;
+}
+
+class QrCode {
+  QrCode(@required this.displayValue,@required this.barcodeCenterVector);
+
+  final String displayValue;
+  final Point barcodeCenterVector; //TODO: Pixels ??
+  late double distanceFromCamera; //Unit: millimetres
+  late Point relativePositionalDifference; //Unit: millimetres
 }
