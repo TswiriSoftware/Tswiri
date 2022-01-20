6f28fc19f2fdea0d6aece69eec1459ce06edbe81
049er <werner@scholtzonline.net>
2022-01-20 08:29:57 +0200

    Test

--- lib/navigation/qrCodeGeneration/qr_code_generation_view.dart
+++ lib/navigation/qrCodeGeneration/qr_code_generation_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_google_ml_kit/navigation/qrCodeGeneration/pdf_creator.dart';
-import 'package:flutter_google_ml_kit/navigation/qrCodeGeneration/pdf_viewer_page.dart';
+import 'package:flutter_google_ml_kit/navigation/qrCodeGeneration/qr_code_viewer.dart';
 import 'package:flutter_google_ml_kit/navigation/qrCodeNavigation/qr_code_navigation.dart';
+import 'package:path_provider/path_provider.dart';
 import '../../main.dart';
 
 class QrCodeGenerationView extends StatefulWidget {
@@ -33,16 +33,16 @@ class _QrCodeGenerationViewState extends State<QrCodeGenerationView> {
           children: [
             CustomCard(
               'Pdf Viewer',
-              PdfViewerPage(path: 'report.pdf'),
+              Calendar(),
               Icons.camera,
               featureCompleted: true,
             ),
-            CustomCard(
-              'Pdf Creator',
-              PDFCreator(),
-              Icons.camera,
-              featureCompleted: true,
-            ),
+            // CustomCard(
+            //   'Pdf Creator',
+            //   PDFCreator(),
+            //   Icons.camera,
+            //   featureCompleted: true,
+            // ),
           ],
         ),
       ),
