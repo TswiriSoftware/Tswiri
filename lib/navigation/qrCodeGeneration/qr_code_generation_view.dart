ff9d6e41731ac07edf08a715505b0db9ba6fa3d1
Spodeopieter <pieter.scholtz@gmail.com>
2022-01-20 08:49:43 +0200

    pdf attemted fix

--- lib/navigation/qrCodeGeneration/qr_code_generation_view.dart
+++ lib/navigation/qrCodeGeneration/qr_code_generation_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_ml_kit/examples/1_6.dart';
 import 'package:flutter_google_ml_kit/navigation/qrCodeGeneration/qr_code_viewer.dart';
 import 'package:flutter_google_ml_kit/navigation/qrCodeNavigation/qr_code_navigation.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:printing/printing.dart';
 import '../../main.dart';
 
 class QrCodeGenerationView extends StatefulWidget {
@@ -31,12 +33,19 @@ class _QrCodeGenerationViewState extends State<QrCodeGenerationView> {
           crossAxisCount: 2,
           // ignore: prefer_const_literals_to_create_immutables
           children: [
-            CustomCard(
-              'Pdf Viewer',
-              Calendar(),
-              Icons.camera,
-              featureCompleted: true,
-            ),
+            PdfPreview(
+        maxPageWidth: 700,
+        build: (format) => generatePDF()),
+        //actions: actions,
+        //onPrinted: _showPrintedToast,
+        //onShared: _showSharedToast,
+            
+            // CustomCard(
+            //   'Pdf Viewer',
+            //   Calendar(),
+            //   Icons.camera,
+            //   featureCompleted: true,
+            // ),
             // CustomCard(
             //   'Pdf Creator',
             //   PDFCreator(),
