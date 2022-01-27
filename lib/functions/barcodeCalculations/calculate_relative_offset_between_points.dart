990fcfb55ffa40cfe5de9ab8aef53e3c0c39b039
049er <werner@scholtzonline.net>
2022-01-27 08:05:19 +0200

    Quadrants

--- lib/functions/barcodeCalculations/calculate_relative_offset_between_points.dart
+++ lib/functions/barcodeCalculations/calculate_relative_offset_between_points.dart
@@ -6,10 +6,17 @@ import 'package:flutter_google_ml_kit/globalValues/global_scaling_factors.dart';
 ///
 /// relativeOffsetBetweenBarcodes = absoluteOffsetBetweenBarcodes * absoluteSizeOfBarcodes * relativeScaleFactor
 ///
-Offset calculateRelativeOffsetBetweenBarcodes(
+Offset translateOffsetAbsoluteToRelative(
     Offset absoluteOffsetBetweenBarcodes, double absoluteSizeOfBarcodes) {
   Offset relativeOffsetBetweenBarcodes = (absoluteOffsetBetweenBarcodes) *
       (absoluteSizeOfBarcodes * relativeScaleFactor);
 
   return relativeOffsetBetweenBarcodes;
+}
+
+Offset translateOffsetRelativeToAbsolute(
+    Offset relativeOffsetBetweenBarcodes, double absoluteSizeOfBarcodes) {
+  Offset absoluteOffsetBetweenBarcodes = (relativeOffsetBetweenBarcodes) /
+      (absoluteSizeOfBarcodes * relativeScaleFactor);
+  return absoluteOffsetBetweenBarcodes;
 }
