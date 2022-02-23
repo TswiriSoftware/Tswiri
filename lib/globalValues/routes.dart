f973cc35e14059c135105801e692c65d782dead7
049er <werner@scholtzonline.net>
2022-02-23 06:51:49 +0200

    routes ?

--- lib/globalValues/routes.dart
+++ lib/globalValues/routes.dart
@@ -4,39 +4,55 @@ import '../main.dart';
 import '../sunbirdViews/barcodeControlPanel/all_barcodes.dart';
 import '../sunbirdViews/barcodeGeneration/barcode_generation_range_selector_view.dart';
 import '../sunbirdViews/barcodeNavigation/navigationToolsView/barcode_navigation_tools_view.dart';
-import '../sunbirdViews/barcodeScanning/barcode_scanner_data_processing.dart';
 import '../sunbirdViews/barcodeScanning/barcode_scanner_view.dart';
 import '../sunbirdViews/barcodeScanning/real_barcode_position_database_view.dart';
 import '../sunbirdViews/barcodeScanning/real_barcode_position_database_visualization_view.dart';
 import '../sunbirdViews/barcodeScanning/scanningToolsView/barcode_scanning_tools_view.dart';
 import '../sunbirdViews/cameraCalibration/calibrationToolsView/camera_calibration_tools_view.dart';
 
+//WIP//
 var allRoutes = <String, WidgetBuilder>{
   //Main Screen
   '/': (context) => const MyApp(),
 
-  //Barcode Scanning//
+  //////////Barcode Scanning//////////
+
   //1. Displays all barcode scanning tools.
   '/BarcodeScanningToolsView': (context) => const BarcodeScanningToolsView(),
   //2. Camera widget that allows for scanning.
   '/BarcodeScannerView': (context) => const BarcodeScannerView(),
-
+  //3. Shows a list of all real barcode positions.
   '/RealBarcodePositionDatabaseView': (context) =>
       const RealBarcodePositionDatabaseView(),
+  //4. Shows a visual representaion of all real barcode Positions
   '/RealBarcodePositionDatabaseVisualizationView': (context) =>
       const RealBarcodePositionDatabaseVisualizationView(),
 
-  //Barcode Navigation
+  ////////////////////////////////////
+
+  /////////Barcode Navigation/////////
+
   '/BarcodeNavigationView': (context) => const BarcodeNavigationView(),
 
-  //Barcode Generation
+  ////////////////////////////////////
+
+  /////////Barcode Generation/////////
+
   '/BarcodeGenerationRangeSelectorView': (context) =>
       const BarcodeGenerationRangeSelectorView(),
 
-  //Barcode Control Panel
+  ////////////////////////////////////
+
+  ///////Barcode Control Panel////////
+
   '/AllBarcodesView': (context) => const AllBarcodesView(),
 
-  //Camera Calibration
+  ////////////////////////////////////
+
+  /////////Camera Calibration/////////
+
   '/CameraCalibrationToolsView': (context) =>
       const CameraCalibrationToolsView(),
+
+  ////////////////////////////////////
 };
