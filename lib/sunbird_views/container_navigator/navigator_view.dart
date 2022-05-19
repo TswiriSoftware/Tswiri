d81820906e755c55c3b69f30045dfadc1c7b9c23
049er <werner@scholtzonline.net>
2022-05-19 12:57:16 +0200

    Problem Fixing

--- lib/sunbird_views/container_navigator/navigator_view.dart
+++ lib/sunbird_views/container_navigator/navigator_view.dart
@@ -11,12 +11,16 @@ import 'package:flutter_google_ml_kit/sunbird_views/container_navigator/isolates
 import 'package:flutter_google_ml_kit/sunbird_views/container_navigator/isolates/messages/image_data.dart';
 import 'package:flutter_google_ml_kit/sunbird_views/container_navigator/isolates/messages/image_processor_config.dart';
 import 'package:flutter_google_ml_kit/sunbird_views/container_navigator/navigator_painter.dart';
+import 'package:flutter_google_ml_kit/sunbird_views/widgets/cards/default_card/defualt_card.dart';
 import 'package:flutter_isolate/flutter_isolate.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'dart:isolate';
 import 'package:vector_math/vector_math.dart' as vm;
-
 import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:isar/isar.dart';
+
+//ImageProcessor Counter.
+int counter = 0;
 
 class NavigatorView extends StatefulWidget {
   const NavigatorView({
@@ -52,9 +56,6 @@ class _NavigatorViewState extends State<NavigatorView> {
   vm.Vector3 accelerometerEvent = vm.Vector3(0, 0, 0);
   vm.Vector3 userAccelerometerEvent = vm.Vector3(0, 0, 0);
 
-  //ImageProcessor Counter.
-  int counter = 0;
-
   //Painter
   bool isBusy = false;
 
@@ -65,6 +66,7 @@ class _NavigatorViewState extends State<NavigatorView> {
   void initState() {
     //Spawn Impage processors.
     initiateIsolates();
+    counter = 0;
 
     //Initaite Accelerometer.
     initiateAccelerometer();
@@ -84,6 +86,7 @@ class _NavigatorViewState extends State<NavigatorView> {
 
   //Initiate Isolates and uiPort listener.
   void initiateIsolates() {
+    FlutterIsolate.killAll();
     FlutterIsolate.spawn(imageProcessor, [
       1, //[0] ID
       uiPort1.sendPort, //[1] SendPort
@@ -131,11 +134,14 @@ class _NavigatorViewState extends State<NavigatorView> {
         showNoGridDialog(context);
 
         break;
-      case 'unkownposition':
+      case 'NoPath':
         initiateDialog(
-          'Unkown Position',
-          'This barcode does not appear in any grids.',
+          'No Path',
+          'Cannot find a path to selected barcode.',
         );
+        break;
+      case 'Path':
+        treeNavigator('Navigator', message[3], message[2]);
         break;
       default:
     }
@@ -158,17 +164,19 @@ class _NavigatorViewState extends State<NavigatorView> {
   Stack _body() {
     return Stack(
       children: [
-        BarcodePositionScannerCameraView(
-          color: color,
-          title: 'Position Scanner',
-          customPaint: customPaint,
-          onImage: (inputImage) {
-            if (hasConfiguredIPs == false) {
-              configureImageProcessors(inputImage);
-            } else {
-              sendImageDataMessage(inputImage);
-            }
-          },
+        RepaintBoundary(
+          child: BarcodePositionScannerCameraView(
+            color: color,
+            title: 'Position Scanner',
+            customPaint: customPaint,
+            onImage: (inputImage) {
+              if (hasConfiguredIPs == false) {
+                configureImageProcessors(inputImage);
+              } else {
+                sendImageDataMessage(inputImage);
+              }
+            },
+          ),
         ),
         Visibility(
           visible: imageProcessor1 == null && imageProcessor2 == null,
@@ -180,14 +188,17 @@ class _NavigatorViewState extends State<NavigatorView> {
     );
   }
 
-  FloatingActionButton _floatingActionButton() {
-    return FloatingActionButton(
-      backgroundColor: color,
-      heroTag: null,
-      onPressed: () {
-        Navigator.pop(context);
-      },
-      child: const Icon(Icons.check_circle_outline_rounded),
+  Visibility _floatingActionButton() {
+    return Visibility(
+      visible: imageProcessor1 != null && imageProcessor2 != null,
+      child: FloatingActionButton(
+        backgroundColor: color,
+        heroTag: null,
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.check_circle_outline_rounded),
+      ),
     );
   }
 
@@ -318,9 +329,128 @@ class _NavigatorViewState extends State<NavigatorView> {
             content: Wrap(
               crossAxisAlignment: WrapCrossAlignment.start,
               children: [
-                Text(message),
+                Text(
+                  message,
+                  style: const TextStyle(fontSize: 15),
+                ),
               ],
             ),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(initialDialogContext);
+                },
+                child: const Text('close'),
+              ),
+            ],
+          );
+        },
+      );
+      setState(() {
+        showingDialog = false;
+      });
+    }
+  }
+
+  void treeNavigator(String title, List<String> selectedBarcodeTree,
+      List<String> currentBarcodeTree) async {
+    if (showingDialog == false) {
+      setState(() {
+        showingDialog = true;
+      });
+      await showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (initialDialogContext) {
+          return AlertDialog(
+            insetPadding: EdgeInsets.all(20),
+            contentPadding: EdgeInsets.all(5),
+            title: Text(title),
+            content: Builder(builder: (context) {
+              ContainerEntry? currentContainer = isarDatabase!.containerEntrys
+                  .filter()
+                  .containerUIDMatches(currentBarcodeTree[0])
+                  .findFirstSync();
+
+              ContainerEntry? selectedContainer = isarDatabase!.containerEntrys
+                  .filter()
+                  .containerUIDMatches(selectedBarcodeTree[0])
+                  .findFirstSync();
+
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        defaultCard(
+                            body: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('You are here:'),
+                                Text(
+                                  'UID',
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                                Text(
+                                    currentContainer?.containerUID ?? 'unkown'),
+                                Text(
+                                  'name',
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                                Text(currentContainer?.name ?? 'no name'),
+                              ],
+                            ),
+                            color: color),
+                        defaultCard(
+                            body: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('You want to go here:'),
+                                Text(
+                                  'UID',
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                                Text(selectedContainer?.containerUID ??
+                                    'unkown'),
+                                Text(
+                                  'name',
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                                Text(selectedContainer?.name ?? 'no name'),
+                              ],
+                            ),
+                            color: color),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        defaultCard(
+                            body: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('Path'),
+                                for (String x in currentBarcodeTree) Text(x)
+                              ],
+                            ),
+                            color: color),
+                        defaultCard(
+                            body: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('Path'),
+                                for (String x in selectedBarcodeTree) Text(x)
+                              ],
+                            ),
+                            color: color),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }),
             actions: [
               ElevatedButton(
                 onPressed: () {
