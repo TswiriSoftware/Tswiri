510a82f6d51be500b2d8691c0c84f8441bae767f
049er <werner@scholtzonline.net>
2022-06-02 08:06:09 +0200

    Removed Unused Imports

--- lib/sunbird_views/containers/container_grid/container_grid_view.dart
+++ lib/sunbird_views/containers/container_grid/container_grid_view.dart
@@ -1,7 +1,5 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_google_ml_kit/global_values/barcode_colors.dart';
-import 'package:flutter_google_ml_kit/global_values/shared_prefrences.dart';
 import 'package:flutter_google_ml_kit/isar_database/containers/container_entry/container_entry.dart';
 import 'package:flutter_google_ml_kit/functions/isar_functions/isar_functions.dart';
 import 'package:flutter_google_ml_kit/isar_database/barcodes/marker/marker.dart';
