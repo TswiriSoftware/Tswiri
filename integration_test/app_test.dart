4aec51991e0044538f91a5bb8ce237203e13c0f4
049er <werner@scholtzonline.net>
2022-06-03 14:22:21 +0200

    tests

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -6,13 +6,14 @@ import 'package:flutter_google_ml_kit/main.dart' as app;
 
 void main() {
   group('Testing App Performance Tests', () {
-    final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-    binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+    var binding = TestWidgetsFlutterBinding.ensureInitialized();
+    if (binding is LiveTestWidgetsFlutterBinding) {
+      binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+    }
 
     testWidgets('Integration Test', (tester) async {
       await app.main();
-      await tester.pumpAndSettle();
+      await tester.pumpAndSettle(const Duration(seconds: 10));
       expect(find.text('Sunbird'), findsOneWidget);
       await tester.tap(find.text('Calibration'));
       await tester.pumpAndSettle(const Duration(milliseconds: 1200));
