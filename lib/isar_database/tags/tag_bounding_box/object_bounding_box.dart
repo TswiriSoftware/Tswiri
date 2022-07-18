81fe73090d7f6c3dab8d46381fb9cdae24287fa3
Werner <werner@scholtzonline.net>
2022-07-18 08:20:25 +0200

    Changes

---
+++ lib/isar_database/tags/tag_bounding_box/object_bounding_box.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+import 'package:isar/isar.dart';
+part 'object_bounding_box.g.dart';
+
+@Collection()
+class ObjectBoundingBox {
+  int id = Isar.autoIncrement;
+
+  ///BoundingBoxID.
+  late int mlTagID;
+
+  ///The boundingBox. use rect.fromLTRB
+  late List<double> boundingBox;
+
+  @override
+  bool operator ==(Object other) {
+    return other is ObjectBoundingBox &&
+        id == other.id &&
+        mlTagID == other.mlTagID &&
+        listEquals(boundingBox, other.boundingBox);
+  }
+
+  @override
+  int get hashCode => id.hashCode;
+
+  @override
+  String toString() {
+    return 'boundingBox: $boundingBox, mlTagID: $mlTagID';
+  }
+
+  Map toJson() => {
+        'id': id,
+        'mlTagID': mlTagID,
+        'boundingBox': boundingBox,
+      };
+
+  ObjectBoundingBox fromJson(Map<String, dynamic> json) {
+    return ObjectBoundingBox()
+      ..id = json['id']
+      ..mlTagID = json['mlTagID']
+      ..boundingBox = (json['boundingBox'] as List<dynamic>).cast<double>();
+  }
+}
