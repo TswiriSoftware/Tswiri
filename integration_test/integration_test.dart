585a2a8b8d068411dc826ac2d7f47524f165e839
Werner <werner@scholtzonline.net>
2022-09-24 08:16:41 +0200

    integration test.

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -4,6 +4,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:tswiri/main.dart' as app;
 
+import 'test_import_function.dart';
+
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
@@ -32,65 +34,6 @@ void main() {
       //TODO: add more tests.
     });
   });
-}
-
-///Import databse and check if it populates the search screen.
-Future<void> testImportFunction(WidgetTester tester) async {
-  await tester.pumpAndSettle(const Duration(seconds: 1));
-  bool foundBetaPopup = tester.any(find.byKey(const Key('noted')));
-
-  if (foundBetaPopup) {
-    await tester.tap(find.byKey(const Key('noted')));
-    await tester.pumpAndSettle();
-  }
-
-  //Check that the tab bar is visible.
-  await tester.pumpAndSettle();
-  expect(find.byKey(const Key('tab_bar')), findsOneWidget);
-  await tester.pumpAndSettle();
-
-  //Navigate to the settigns tab
-  await tester.tap(find.byKey(
-    const Key('settings_tab'),
-  ));
-  await tester.pumpAndSettle();
-
-  //Navigate to manage backups.
-  await tester.tap(find.byKey(
-    const Key('manage_backup_view'),
-  ));
-  await tester.pumpAndSettle();
-
-  //Navigate to manual backup.
-  await tester.tap(find.byKey(
-    const Key('manual_backup'),
-  ));
-  await tester.pumpAndSettle();
-
-  //Tap hidden button to import asset.
-  await tester.tap(find.byKey(
-    const Key('import_from_asset'),
-  ));
-  await tester.pumpAndSettle(const Duration(seconds: 2));
-
-  //Go back to Backup Options.
-  await tester.pageBack();
-  await tester.pumpAndSettle();
-
-  //Go back to Settings.
-  await tester.pageBack();
-  await tester.pumpAndSettle();
-
-  //Navigate back to the search tab.
-  await tester.tap(find.byKey(
-    const Key('search_tab'),
-  ));
-  await tester.pumpAndSettle();
-
-  await tester.pump(const Duration(seconds: 1));
-
-  expect(find.text('Shelf 1'), findsOneWidget);
-  expect(find.text('Box 1'), findsOneWidget);
 }
 
 ///Test Containers view.
