0ecdd2d2a1544c234c56793f8f6705eabd6088ea
Werner <werner@scholtzonline.net>
2022-09-29 08:05:48 +0200

    Patrol test

--- integration_test/patrol_test.dart
+++ integration_test/patrol_test.dart
@@ -1,73 +1,103 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:patrol/patrol.dart';
-import 'package:tswiri/main.dart' as app;
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:patrol/patrol.dart';
+// import 'package:tswiri/main.dart' as app;
 
-import 'patrol_config.dart';
+// import 'patrol_config.dart';
 
-void main() {
-  patrolTest(
-    config: patrolConfig,
-    nativeAutomation: true,
-    'description',
-    ($) async {
-      await app.main();
-      // await $.pumpWidgetAndSettle(const MyApp());
-      await $.pumpAndSettle(const Duration(seconds: 1));
-      bool foundBetaPopup = $.tester.any(find.byKey(const Key('noted')));
+// void main() {
+//   patrolTest(
+//     config: patrolConfig,
+//     nativeAutomation: true,
+//     'description',
+//     ($) async {
+//       await app.main();
+//       // await $.pumpWidgetAndSettle(const MyApp());
+//       await $.pumpAndSettle(const Duration(seconds: 1));
+//       bool foundBetaPopup = $.tester.any(find.byKey(const Key('noted')));
 
-      if (foundBetaPopup) {
-        await $.tap(find.byKey(const Key('noted')));
-        await $.pumpAndSettle();
-      }
+//       if (foundBetaPopup) {
+//         await $.tap(find.byKey(const Key('noted')));
+//         await $.pumpAndSettle();
+//       }
 
-      //Navigate to the settigns tab.
-      await $.tap(find.byKey(
-        const Key('settings_tab'),
-      ));
-      await $.pumpAndSettle();
+//       //Test Import Functionality.
+//       // await testImport($);
 
-      //Navigate to manage backups.
-      await $.tap(find.byKey(
-        const Key('manage_backup_view'),
-      ));
-      await $.pumpAndSettle();
+//       //Navigate to the settigns tab.
+//       await $.tap(find.byKey(
+//         const Key('settings_tab'),
+//       ));
+//       await $.pumpAndSettle();
 
-      //Navigate to manual backup.
-      await $.tap(find.byKey(
-        const Key('manual_backup'),
-      ));
-      await $.pumpAndSettle();
+//       //Navigate to manage backups.
+//       await $.tap(find.byKey(
+//         const Key('manage_backup_view'),
+//       ));
 
-      //Tap 'Restore Backup' button.
-      await $.tap(find.byKey(const Key('restore_backup')));
+//       //Navigate to manual backup.
+//       await $.tap(find.byKey(
+//         const Key('google_drive_backup'),
+//       ));
+//       await $.pumpAndSettle();
 
-      //Tap main_space_test_V1.zip in file explorer.
-      await $.native.tap(const Selector(textContains: 'main_space_te'));
-      await $.pumpAndSettle();
+//       await $.tap(find.byIcon(Icons.login));
+//       await $.pumpAndSettle();
 
-      //Tap 'Restore' button.
-      await $.tap(find.text('Restore'));
+//       await $.native.tap(const Selector(textContains: 'Werner'));
+//       await Future.delayed(const Duration(seconds: 2));
+//       await $.pumpAndSettle();
+//       expect(find.text('Last Backup'), findsOneWidget);
 
-      //Wait for the backup to restore.
-      await Future.delayed(const Duration(seconds: 5));
+//       await $.pumpAndSettle(const Duration(seconds: 5));
+//     },
+//   );
+// }
 
-      //Navigate back to 'Backup Options'
-      await $.tap(find.byIcon(Icons.arrow_back));
+// Future<void> testImport(PatrolTester $) async {
+//   //Navigate to the settigns tab.
+//   await $.tap(find.byKey(
+//     const Key('settings_tab'),
+//   ));
+//   await $.pumpAndSettle();
 
-      //Navigate back to 'Settings'
-      await $.tap(find.byIcon(Icons.arrow_back));
+//   //Navigate to manage backups.
+//   await $.tap(find.byKey(
+//     const Key('manage_backup_view'),
+//   ));
+//   await $.pumpAndSettle();
 
-      // //Navigate to the search tab.
-      await $.tap(find.byKey(
-        const Key('search_tab'),
-      ));
-      await $.pumpAndSettle();
+//   //Navigate to manual backup.
+//   await $.tap(find.byKey(
+//     const Key('manual_backup'),
+//   ));
+//   await $.pumpAndSettle();
 
-      expect(find.text('Shelf 1'), findsOneWidget);
-      expect(find.text('Box 1'), findsOneWidget);
+//   //Tap 'Restore Backup' button.
+//   await $.tap(find.byKey(const Key('restore_backup')));
 
-      await $.pumpAndSettle(const Duration(seconds: 5));
-    },
-  );
-}
+//   //Tap main_space_test_V1.zip in file explorer.
+//   await $.native.tap(const Selector(textContains: 'main_space_te'));
+//   await $.pumpAndSettle();
+
+//   //Tap 'Restore' button.
+//   await $.tap(find.text('Restore'));
+
+//   //Wait for the backup to restore.
+//   await Future.delayed(const Duration(seconds: 5));
+
+//   //Navigate back to 'Backup Options'
+//   await $.tap(find.byIcon(Icons.arrow_back));
+
+//   //Navigate back to 'Settings'
+//   await $.tap(find.byIcon(Icons.arrow_back));
+
+//   // //Navigate to the search tab.
+//   await $.tap(find.byKey(
+//     const Key('search_tab'),
+//   ));
+//   await $.pumpAndSettle();
+
+//   expect(find.text('Shelf 1'), findsOneWidget);
+//   expect(find.text('Box 1'), findsOneWidget);
+// }
